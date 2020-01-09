80688c759bdd06a92680d4a7052f9216f418c260
CheeseGrove <zit1432@hotmail.com>
2020-01-09 15:15:04 +0100

    password

--- test/src/App.js
+++ test/src/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-
-
-
-
 function App() {
+
+  const [num, setNum] = useState(null);
+  const [showSecret, setShowSecret] = useState(false);
+  const [wrongPass, setWrongPass] = useState(false);
+
+  function doSomething() {
+    
+    if(num == "puma") {
+      setShowSecret(true);
+      setWrongPass(false);
+    } else {
+      setWrongPass(true);
+      setShowSecret(false);
+    }
+    
+  }
+
+  function secretMessage() {
+    return (
+      <p>UwU chan</p>
+    );
+  }
+  function wrongPassword() {
+    return(
+      <p>Wrong Password</p>
+    );
+  }
+
+  function handleChange(event) {
+    setNum(event.target.value);
+  }
+
   return (
-    <div>Hellu UwU</div>
+    <div>
+      <input type="text" placeholder="Password" value={num} onChange={handleChange}></input>
+      <input value="Button" type="submit" onClick={doSomething}></input>
+      <div>{showSecret ? secretMessage() : null}</div>
+      <div>{wrongPass ? wrongPassword() : null}</div>
+    </div>
   );
 }
 
